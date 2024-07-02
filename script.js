1dba01bef68a199694c0094d12647171d932a6d3
fikratkhatira <fekratkhatira@gmail.com>
2024-07-01 20:46:08 -0700

    css and js second error fixed

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ function convertToRoman() {
 
   const romanSymbols = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
   const romanValues = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-  let num = parseInt(number.value);
+  
+  let num = parseInt(number.value, 10);
   let romanNumeral = '';
 
   for (let i = 0; i < romanValues.length; i++) {
